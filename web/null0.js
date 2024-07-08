9be6842de0f194dd9ff2a913d5541adebf4ecab3
David Konsumer <konsumer@jetboystudio.com>
2024-07-07 23:49:32 -0700

    shared mem does not work. reverting

--- web/null0.js
+++ web/null0.js
@@ -1,7 +1,31 @@
 import loadCart from '@null0/browser'
 import { unzip } from 'unzipit'
+import memhelpers from 'cmem_helpers'
+import wireCartToHost from './null0_wasm.js'
 
-// this is GamePadButton enum
+/*
+typedef enum GamepadButton {
+  GAMEPAD_BUTTON_UNKNOWN = 0,     // Unknown button, just for error checking
+  GAMEPAD_BUTTON_UP = 1,          // Gamepad left DPAD up button
+  GAMEPAD_BUTTON_RIGHT = 2,           // Gamepad left DPAD right button
+  GAMEPAD_BUTTON_DOWN = 3,            // Gamepad left DPAD down button
+  GAMEPAD_BUTTON_LEFT = 4,            // Gamepad left DPAD left button
+  GAMEPAD_BUTTON_Y = 5,               // Gamepad right button up (i.e. PS3: Triangle, Xbox: Y)
+  GAMEPAD_BUTTON_B = 6,               // Gamepad right button right (i.e. PS3: Square, Xbox: X)
+  GAMEPAD_BUTTON_A = 7,               // Gamepad right button down (i.e. PS3: Cross, Xbox: A)
+  GAMEPAD_BUTTON_X = 8,               // Gamepad right button left (i.e. PS3: Circle, Xbox: B)
+  GAMEPAD_BUTTON_LEFT_SHOULDER = 9,   // Gamepad top/back trigger left (first), it could be a trailing button
+  GAMEPAD_BUTTON_LEFT_TRIGGER = 10,    // Gamepad top/back trigger left (second), it could be a trailing button
+  GAMEPAD_BUTTON_RIGHT_SHOULDER = 11,  // Gamepad top/back trigger right (one), it could be a trailing button
+  GAMEPAD_BUTTON_RIGHT_TRIGGER = 12,   // Gamepad top/back trigger right (second), it could be a trailing button
+  GAMEPAD_BUTTON_SELECT = 13,          // Gamepad center buttons, left one (i.e. PS3: Select)
+  GAMEPAD_BUTTON_MENU = 14,            // Gamepad center buttons, middle one (i.e. PS3: PS, Xbox: XBOX)
+  GAMEPAD_BUTTON_START = 15,           // Gamepad center buttons, right one (i.e. PS3: Start)
+  GAMEPAD_BUTTON_LEFT_THUMB = 16,      // Gamepad joystick pressed button left
+  GAMEPAD_BUTTON_RIGHT_THUMB = 17,     // Gamepad joystick pressed button right
+} GamepadButton;
+*/
+
 export const Buttons = {
   A: 7,
   B: 6,
@@ -17,12 +41,33 @@ export const Buttons = {
   RIGHT: 2
 }
 
-// this will take an emscripten host, and setup imports for plain-wasm cart
-export function getImports(host) {
-  const out = {
+export async function getHost (cartUrl, canvas = document.body.appendChild(document.createElement('canvas'))) {
+  const host = await loadCart({
+    canvas,
+    preRun: async function ({ FS }) {
+      await FS.createPreloadedFile('/', 'cart', cartUrl, true, false)
+    },
+    // for some reason it strips off .null0
+    arguments: ['/cart']
+  })
+
+  const helpers = memhelpers(host.HEAPU8, host._malloc)
+  return { helpers, ...host }
+}
+
+export async function setupCart (url, canvas = document.body.appendChild(document.createElement('canvas'))) {
+  const host = await getHost(url, canvas)
+  const out = { host }
+  const cart = {}
+
+  canvas.width = 320
+  canvas.height = 240
+
+  const imports = {
+    null0: wireCartToHost(host, cart),
+
+    // minimal WASI that only allows console logging
     wasi_snapshot_preview1: {
-      args_get() {},
-      args_sizes_get() {},
       fd_write (fd, iovsPtr, iovsLength, bytesWrittenPtr) {
         const iovs = new Uint32Array(cart.memory.buffer, iovsPtr, iovsLength * 2)
         if (fd === 1 || fd === 2) { // stdout/stderr
@@ -57,163 +102,134 @@ export function getImports(host) {
       fd_seek () {
         return 0
       },
-      proc_exit() {}
-    },
-    
-    env: {
-      memory: host.wasmMemory
-    },
 
-    null0: {}
-  }
-
-  for (let n of Object.keys(host)) {
-    const m = /^_null0_(.+)/g.exec(n)
-    if (m){
-      out.null0[m[1]] = host[n]
+      proc_exit (status) {
+        console.error('exit', status)
+        return 0
+      }
     }
   }
 
-  // here I can wrap to debug
-  out.null0.load_image = s => {
-    const o = host._null0_load_image(s)
-    console.log('load_image', host.UTF8ToString(s), o)
-    return o
+  const { entries } = await unzip(url)
+
+  // cart could be a plain zip file for js filesystem
+  if (entries['main.wasm']) {
+    const wasmBytes = await entries['main.wasm'].arrayBuffer()
+    const cartMod = await WebAssembly.compile(wasmBytes)
+    const { exports } = await WebAssembly.instantiate(cartMod, imports)
+
+    for (const k of Object.keys(exports)) {
+      cart[k] = exports[k]
+    }
+
+    const helpers = memhelpers(cart.memory.buffer, cart.malloc)
+    cart.getString = helpers.getString
+    cart.setString = helpers.setString
+    cart.struct = helpers.struct
+    cart.view = new DataView(cart.memory.buffer)
+  }
+
+  out.cart = cart
+
+  if (cart._start) {
+    cart._start()
+  }
+
+  if (cart.load) {
+    cart.load()
+  }
+
+  if (cart.buttonDown) {
+    window.addEventListener('keydown', ({key}) => {
+      if (key === 'z') {
+        cart.buttonDown(Buttons.B)
+      }
+      if (key === 'x') {
+        cart.buttonDown(Buttons.A)
+      }
+      if (key === 'a') {
+        cart.buttonDown(Buttons.Y)
+      }
+      if (key === 's') {
+        cart.buttonDown(Buttons.X)
+      }
+      if (key === 'Shift') {
+        cart.buttonDown(Buttons.SELECT)
+      }
+      if (key === 'Enter') {
+        cart.buttonDown(Buttons.START)
+      }
+      if (key === 'q') {
+        cart.buttonDown(Buttons.L)
+      }
+      if (key === 'w') {
+        cart.buttonDown(Buttons.R)
+      }
+      if (key === 'ArrowUp') {
+        cart.buttonDown(Buttons.UP)
+      }
+      if (key === 'ArrowDown') {
+        cart.buttonDown(Buttons.DOWN)
+      }
+      if (key === 'ArrowLeft') {
+        cart.buttonDown(Buttons.LEFT)
+      }
+      if (key === 'ArrowRight') {
+        cart.buttonDown(Buttons.RIGHT)
+      }
+    })
+  }
+
+  if (cart.buttonUp) {
+    window.addEventListener('keyup', ({key}) => {
+      if (key === 'z') {
+        cart.buttonUp(Buttons.B)
+      }
+      if (key === 'x') {
+        cart.buttonUp(Buttons.A)
+      }
+      if (key === 'a') {
+        cart.buttonUp(Buttons.Y)
+      }
+      if (key === 's') {
+        cart.buttonUp(Buttons.X)
+      }
+      if (key === 'Shift') {
+        cart.buttonUp(Buttons.SELECT)
+      }
+      if (key === 'Enter') {
+        cart.buttonUp(Buttons.START)
+      }
+      if (key === 'q') {
+        cart.buttonUp(Buttons.L)
+      }
+      if (key === 'w') {
+        cart.buttonUp(Buttons.R)
+      }
+      if (key === 'ArrowUp') {
+        cart.buttonUp(Buttons.UP)
+      }
+      if (key === 'ArrowDown') {
+        cart.buttonUp(Buttons.DOWN)
+      }
+      if (key === 'ArrowLeft') {
+        cart.buttonUp(Buttons.LEFT)
+      }
+      if (key === 'ArrowRight') {
+        cart.buttonUp(Buttons.RIGHT)
+      }
+    })
+  }
+
+  if (cart.update) {
+    const cartUpdate = () => {
+      cart.update()
+      window.requestAnimationFrame(cartUpdate)
+    }
+    window.requestAnimationFrame(cartUpdate)
   }
 
   return out
 }
 
-// load cart as file, load host
-export async function getHost (cartUrl, canvas = document.body.appendChild(document.createElement('canvas'))) {
-  const host = await loadCart({
-    canvas,
-    preRun: async function ({ FS }) {
-      await FS.createPreloadedFile('/', 'cart', cartUrl, true, false)
-    },
-    // for some reason it strips off .null0
-    arguments: ['/cart']
-  })
-  canvas.width = 320
-  canvas.height = 240
-  return host
-}
-
-// entry-point for web-stuff
-export async function setupCart(cartUrl, canvas = document.body.appendChild(document.createElement('canvas'))) {
-  const host = await getHost(cartUrl, canvas)
-  const importObj = getImports(host)
-  const { entries } = await unzip(cartUrl)
-  if (entries['main.wasm']) {
-    const wasmBytes = await entries['main.wasm'].arrayBuffer()
-    const cartMod = await WebAssembly.compile(wasmBytes)
-    const cart = {...(await WebAssembly.instantiate(cartMod, importObj)).exports, imports: importObj}
-
-    // console.log({cart, host})
-
-    if (cart._start) {
-      cart._start()
-    }
-
-    if (cart.load) {
-      cart.load()
-    }
-
-    if (cart.update) {
-      const cartUpdate = () => {
-        cart.update()
-        window.requestAnimationFrame(cartUpdate)
-      }
-      window.requestAnimationFrame(cartUpdate)
-    }
-
-    // TODO: add real joystick handler here, too
-
-    if (cart.buttonDown) {
-      window.addEventListener('keydown', ({key}) => {
-        if (key === 'z') {
-          cart.buttonDown(Buttons.B)
-        }
-        if (key === 'x') {
-          cart.buttonDown(Buttons.A)
-        }
-        if (key === 'a') {
-          cart.buttonDown(Buttons.Y)
-        }
-        if (key === 's') {
-          cart.buttonDown(Buttons.X)
-        }
-        if (key === 'Shift') {
-          cart.buttonDown(Buttons.SELECT)
-        }
-        if (key === 'Enter') {
-          cart.buttonDown(Buttons.START)
-        }
-        if (key === 'q') {
-          cart.buttonDown(Buttons.L)
-        }
-        if (key === 'w') {
-          cart.buttonDown(Buttons.R)
-        }
-        if (key === 'ArrowUp') {
-          cart.buttonDown(Buttons.UP)
-        }
-        if (key === 'ArrowDown') {
-          cart.buttonDown(Buttons.DOWN)
-        }
-        if (key === 'ArrowLeft') {
-          cart.buttonDown(Buttons.LEFT)
-        }
-        if (key === 'ArrowRight') {
-          cart.buttonDown(Buttons.RIGHT)
-        }
-      })
-    }
-
-    if (cart.buttonUp) {
-      window.addEventListener('keyup', ({key}) => {
-        if (key === 'z') {
-          cart.buttonUp(Buttons.B)
-        }
-        if (key === 'x') {
-          cart.buttonUp(Buttons.A)
-        }
-        if (key === 'a') {
-          cart.buttonUp(Buttons.Y)
-        }
-        if (key === 's') {
-          cart.buttonUp(Buttons.X)
-        }
-        if (key === 'Shift') {
-          cart.buttonUp(Buttons.SELECT)
-        }
-        if (key === 'Enter') {
-          cart.buttonUp(Buttons.START)
-        }
-        if (key === 'q') {
-          cart.buttonUp(Buttons.L)
-        }
-        if (key === 'w') {
-          cart.buttonUp(Buttons.R)
-        }
-        if (key === 'ArrowUp') {
-          cart.buttonUp(Buttons.UP)
-        }
-        if (key === 'ArrowDown') {
-          cart.buttonUp(Buttons.DOWN)
-        }
-        if (key === 'ArrowLeft') {
-          cart.buttonUp(Buttons.LEFT)
-        }
-        if (key === 'ArrowRight') {
-          cart.buttonUp(Buttons.RIGHT)
-        }
-      })
-    }
-
-    return {cart, host}
-  }
-}
-
 export default setupCart
-
---
+++ web/null0_wasm.js
@@ -0,0 +1,250 @@
+// this will eventually be generated
+
+const MAX_STRING_LEN = 1024 * 20
+
+export default function wireCartToHost (host, cart) {
+  let cartMem
+
+  const copyBytesFromCart = (cartPtr, size, retPtr) => {
+    cartMem ||= new Uint8Array(cart.memory.buffer)
+    const hostPtr = retPtr || host._malloc(size)
+    for (let b = 0; b < size; b++) {
+      host.HEAPU8[hostPtr + b] = cartMem[cartPtr + b]
+    }
+    return hostPtr
+  }
+
+  const copyBytesToCart = (hostPtr, size, retPtr) => {
+    cartMem ||= new Uint8Array(cart.memory.buffer)
+    const cartPtr = retPtr || cart.malloc(size)
+    for (let b = 0; b < size; b++) {
+      cartMem[cartPtr + b] = host.HEAPU8[hostPtr + b]
+    }
+    return cartPtr
+  }
+
+  const cartStrlen = (pointer) => {
+    cartMem ||= new Uint8Array(cart.memory.buffer)
+    let end = pointer
+    while (end < (pointer + MAX_STRING_LEN)) {
+      if (cartMem[end] === 0) {
+        break
+      }
+      end++
+    }
+    return end - pointer
+  }
+
+  const hostStrlen = (pointer) => {
+    let end = pointer
+    while (end < (pointer + MAX_STRING_LEN)) {
+      if (host.HEAPU8[end] === 0) {
+        break
+      }
+      end++
+    }
+    return end - pointer
+  }
+
+  return {
+    trace (strPtr) {
+      console.log(cart.getString(strPtr))
+    },
+
+    image_gradient (width, height, topLeftPtr, topRightPtr, bottomLeftPtr, bottomRightPtr) {
+      const hostTopLeftPtr = copyBytesFromCart(topLeftPtr, 4)
+      const hostTopRightPtr = copyBytesFromCart(topRightPtr, 4)
+      const hostBottomLeftPtr = copyBytesFromCart(bottomLeftPtr, 4)
+      const hostBottomRightPtr = copyBytesFromCart(bottomRightPtr, 4)
+      const r = host._null0_image_gradient(width, height, hostTopLeftPtr, hostTopRightPtr, hostBottomLeftPtr, hostBottomRightPtr)
+      host._free(hostTopLeftPtr)
+      host._free(hostTopRightPtr)
+      host._free(hostBottomLeftPtr)
+      host._free(hostBottomRightPtr)
+      return r
+    },
+
+    clear (colorPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_clear(hostColorPtr)
+      host._free(hostColorPtr)
+    },
+
+    current_time() {
+      return BigInt(Date.now())
+    },
+
+    draw_ellipse (centerX, centerY, radiusX, radiusY, colorPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_draw_ellipse(centerX, centerY, radiusX, radiusY, hostColorPtr)
+      host._free(hostColorPtr)
+    },
+
+    draw_triangle (x1, y1, x2, y2, x3, y3, colorPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_draw_triangle(x1, y1, x2, y2, x3, y3, hostColorPtr)
+      host._free(hostColorPtr)
+    },
+
+    draw_circle (centerX, centerY, radius, colorPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_draw_circle(centerX, centerY, radius, hostColorPtr)
+      host._free(hostColorPtr)
+    },
+
+    draw_rectangle (posX, posY, width, height, colorPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_draw_rectangle(posX, posY, width, height, hostColorPtr)
+      host._free(hostColorPtr)
+    },
+
+    draw_rectangle_outline (posX, posY, width, height, colorPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_draw_rectangle_outline(posX, posY, width, height, hostColorPtr)
+      host._free(hostColorPtr)
+    },
+
+    draw_circle_outline (centerX, centerY, radius, colorPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_draw_circle_outline(centerX, centerY, radius, hostColorPtr)
+      host._free(hostColorPtr)
+    },
+
+    draw_point (x, y, colorPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_draw_point(x, y, hostColorPtr)
+      host._free(hostColorPtr)
+    },
+
+    draw_line (startPosX, startPosY, endPosX, endPosY, colorPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_draw_line(startPosX, startPosY, endPosX, endPosY, hostColorPtr)
+      host._free(hostColorPtr)
+    },
+
+    draw_image (src, posX, posy) {
+      host._null0_draw_image(src, posX, posy)
+    },
+
+    color_tint (retPtr, colorPtr, tintPtr) {
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      const hostTintPtr = copyBytesFromCart(colorPtr, 4)
+      const hostRetPtr = host._malloc(4)
+      host._null0_color_tint(hostRetPtr, hostColorPtr, hostTintPtr)
+      copyBytesToCart(hostRetPtr, 4, retPtr)
+      host._free(hostColorPtr)
+      host._free(hostTintPtr)
+      host._free(hostRetPtr)
+    },
+
+    measure_text (retPtr, font, textPtr) {
+      const len = cartStrlen(textPtr)
+      const textHostPtr = copyBytesFromCart(textPtr, len)
+      const retHostPtr = host._malloc(8)
+      host._null0_measure_text(retHostPtr, font, textHostPtr)
+      copyBytesToCart(retHostPtr, 8, retPtr)
+      host._free(textHostPtr)
+      host._free(retHostPtr)
+    },
+
+    load_image (filenamePtr) {
+      const filenameHostPtr = copyBytesFromCart(filenamePtr, cartStrlen(filenamePtr) + 1)
+      const index = host._null0_load_image(filenameHostPtr)
+      host._free(filenameHostPtr)
+      return index
+    },
+
+    draw_text (font, textPtr, posX, posY, colorPtr) {
+      const textHostPtr = copyBytesFromCart(textPtr, cartStrlen(textPtr) + 1)
+      const hostColorPtr = copyBytesFromCart(colorPtr, 4)
+      host._null0_draw_text(font, textHostPtr, posX, posY, hostColorPtr)
+      host._free(textHostPtr)
+      host._free(hostColorPtr)
+    },
+
+    load_font_ttf (filenamePtr, fontSize) {
+      const filenameHostPtr = copyBytesFromCart(filenamePtr, cartStrlen(filenamePtr) + 1)
+      const index = host._null0_load_font_ttf(filenameHostPtr, fontSize)
+      host._free(filenameHostPtr)
+      return index
+    },
+
+    // u32 load_font_tty(char* filename, i32 glyphWidth, i32 glyphHeight, char* characters);
+
+    load_font_tty(filenamePtr, glyphWidth, glyphHeight, charactersPtr){
+      const filenameHostPtr = copyBytesFromCart(filenamePtr, cartStrlen(filenamePtr) + 1)
+      const charactersHostPtr = copyBytesFromCart(charactersPtr, cartStrlen(charactersPtr) + 1)
+      const index = host._null0_load_font_tty(filenameHostPtr, glyphWidth, glyphHeight, charactersHostPtr)
+      host._free(filenameHostPtr)
+      host._free(charactersHostPtr)
+      return index
+    },
+
+    load_sound (filenamePtr) {
+      const filenameHostPtr = copyBytesFromCart(filenamePtr, cartStrlen(filenamePtr) + 1)
+      const index = host._null0_load_sound(filenameHostPtr)
+      host._free(filenameHostPtr)
+      return index
+    },
+
+    play_sound (sound, loop) {
+      host._null0_play_sound(sound, loop)
+    },
+
+    unload_sound (sound) {
+      host._null0_unload_sound(sound)
+    },
+
+    get_write_dir () {
+      const hostRetPtr = host._null0_get_write_dir()
+      const len = hostStrlen(hostRetPtr)
+      return copyBytesToCart(hostRetPtr, len)
+    },
+
+    file_read (filenamePtr, bytesReadPtr) {
+      const filenameHostPtr = copyBytesFromCart(filenamePtr, cartStrlen(filenamePtr) + 1)
+      const bytesReadHostPtr = host._malloc(4)
+      const hostRetPtr = host._null0_file_read(filenameHostPtr, bytesReadHostPtr)
+      copyBytesToCart(bytesReadHostPtr, 4, bytesReadPtr)
+      const len = host.HEAPU32[bytesReadHostPtr / 4]
+      const ret = copyBytesToCart(hostRetPtr, len)
+      host._free(filenameHostPtr)
+      host._free(bytesReadHostPtr)
+      host._free(hostRetPtr)
+      return ret
+    },
+
+    file_info (retPtr, filenamePtr) {
+      const retHostPtr = host._malloc(96)
+      const filenameHostPtr = copyBytesFromCart(filenamePtr, cartStrlen(filenamePtr) + 1)
+      host._null0_file_info(retHostPtr, filenameHostPtr)
+      copyBytesToCart(retHostPtr, 96, retPtr)
+      host._free(retHostPtr)
+      host._free(filenameHostPtr)
+    },
+
+    file_write (filenamePtr, bytesPtr, byteSize) {
+      const filenameHostPtr = copyBytesFromCart(filenamePtr, cartStrlen(filenamePtr) + 1)
+      const bytesHostPtr = copyBytesFromCart(bytesPtr, byteSize)
+      copyBytesFromCart(bytesHostPtr, byteSize, bytesPtr)
+      const ret = host._null0_file_write(filenameHostPtr, bytesHostPtr, byteSize)
+      host._free(filenameHostPtr)
+      host._free(bytesHostPtr)
+      return ret
+    },
+
+    new_sfx (paramsPtr) {
+      const paramsHostPtr = copyBytesFromCart(paramsPtr, 48)
+      const ret = host._null0_new_sfx(paramsHostPtr)
+      host._free(paramsHostPtr)
+      return ret
+    },
+
+    preset_sfx (paramsPtr, type) {
+      const paramsHostPtr = copyBytesFromCart(paramsPtr, 48)
+      host._null0_new_sfx(paramsHostPtr, type)
+      copyBytesToCart(paramsPtr, 48, paramsHostPtr)
+      host._free(paramsHostPtr)
+    }
+  }
+}
