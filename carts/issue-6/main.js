0547656b84295671fda1ba4b45e9f39cd8ad47aa
David Konsumer <konsumer@jetboystudio.com>
2025-06-23 00:53:19 -0700

    fix js demo

--- carts/issue-6/main.js
+++ carts/issue-6/main.js
@@ -1,10 +1,15 @@
+let logo
+
 export function load() {
-  const logo = load_image('logo.png')
+  logo = load_image('logo.png')
   const before = measure_image(logo)
-  const logo2 = image_resize(logo, 2, 2, FILTER_NEARESTNEIGHBOR)
+
+  const l = image_scale(logo, 2, 2, FILTER_NEARESTNEIGHBOR)
+  unload_image(logo)
+  logo = l
+
   const after = measure_image(logo)
-  const after2 = measure_image(logo2)
 
   // these should be different
-  console.log(JSON.stringify({ logo, logo2, before, after, after2 }))
+  console.log(JSON.stringify({ logo, before, after }))
 }
