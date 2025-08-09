818e9ac4227e8a88c4c140d8629aafb0c11b55f7
David Konsumer <konsumer@jetboystudio.com>
2025-08-08 18:46:55 -0700

    Windows: define WIN32_LEAN_AND_MEAN and NOMINMAX before including windows.h in exists_next_to_executable.h to minimize Win32 headers and avoid symbol conflicts.

--- host/src/exists_next_to_executable.h
+++ host/src/exists_next_to_executable.h
@@ -4,6 +4,12 @@
 #include <sys/stat.h>
 
 #ifdef _WIN32
+    #ifndef WIN32_LEAN_AND_MEAN
+      #define WIN32_LEAN_AND_MEAN
+    #endif
+    #ifndef NOMINMAX
+      #define NOMINMAX
+    #endif
     #include <windows.h>
     #include <io.h>
     #define PATH_SEPARATOR '\\'
