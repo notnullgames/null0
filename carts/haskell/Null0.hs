6e9d399eeaee8aa74ecbe5098fdbead7470e63d3
David Konsumer <konsumer@jetboystudio.com>
2026-08-31 18:51:19 -0700

    0.0.15

--- carts/haskell/Null0.hs
+++ carts/haskell/Null0.hs
@@ -233,7 +233,7 @@ raywhite = Color 245 245 245 255
 
 -- | The current tagged version of null0 engine
 null0Version :: String
-null0Version = "0.0.14"
+null0Version = "0.0.15"
 
 -- Potential image-filtering techniques for scale/etc.
 filterNearestneighbor :: Int32
